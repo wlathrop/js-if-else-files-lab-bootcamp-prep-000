22e74b18d1697b69d409d03ec7bb240391f3c16c
William Lathrop <wlathrop10@gmail.com>
2018-08-02 19:24:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 var name = "Joe"
 var height = "74"
-if (name=== "Susan"){
-  console.log("The name is correct")
-}else {
-  console.log("Expected 
+
 // Don't worry about this
 module.exports = { name, height
 }
